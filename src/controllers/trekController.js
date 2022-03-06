5912b54062d8a5cea0d55210d374c94da0105291
solealdao <soledad_aldao@hotmail.com>
2022-03-06 20:41:19 -0300

    Detail view

--- src/controllers/trekController.js
+++ src/controllers/trekController.js
@@ -5,7 +5,11 @@ const path = require('path');
 let trekController = {
     create: function (req, res) {
         res.render(path.resolve(__dirname,"../views/treks/trekCreate.ejs"))
-        }
+        },
+    
+    detail: function(req, res) {
+        res.render(path.resolve(__dirname,"../views/treks/trekDetail.ejs"))
+    }
 }
 
 
--- src/routes/treks.js
+++ src/routes/treks.js
@@ -5,5 +5,8 @@ let trekController = require('../controllers/trekController');
 // Ruta a vista Create
 routerTreks.get("/", trekController.create);
 
+// Ruta a vista Detail
+routerTreks.get("/detail", trekController.detail);
+
 
 module.exports = routerTreks;
